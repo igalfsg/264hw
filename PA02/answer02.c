49a57ca05b53a09e5ac3d6d5f75483885c3507ed
Igal Flegmann Sandler <iflegman@ecegrid-thin1.ecn.purdue.edu>
2014-09-08 15:20:30 -0400

    function 2 created

--- PA02/answer02.c
+++ PA02/answer02.c
@@ -4,9 +4,24 @@
 size_t my_strlen (const char * str)
 {
   int count = 0;
-  while (str [count] != "0/")
+  while (str [count] != '\0')
     {
       count++;
+    }
+  return count;
+}
+
+int my_countchar (const char * str, char ch)
+{
+  int count = 0;
+  int index = 0;
+  while (str [index] != '\0')
+    {
+      if (str[index] == ch)
+	{ 
+	  count++;
+	}
+      index ++;
     }
   return count;
 }
