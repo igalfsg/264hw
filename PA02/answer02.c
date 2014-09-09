bc6ea4363d29da30895905cf3f0e4c66401f4a2c
Igal Flegmann Sandler <iflegman@ecegrid-thin1.ecn.purdue.edu>
2014-09-09 11:34:50 -0400

    dded more functions

--- PA02/answer02.c
+++ PA02/answer02.c
@@ -26,7 +26,58 @@ int my_countchar (const char * str, char ch)
   return count;
 }
 
-char * my_strchr (const * str, int ch)
+char * my_strchr (const char * str, int ch)
 {
-  
+  // if ch is 
+  char * point = NULL;
+  int index = 0;
+  while (str[index] != '\0')
+    {
+      if (str[index] == ch)
+	{ 
+	  point = (char *)&str[index];
+	  return point;
+	}
+      index ++;
+    }
+  if (ch == '\0')
+    {
+     point = (char *)&str[index];
+     return point;
+    } 
+  return point;
 }
+
+char * my_strrchr (const char * str, int ch)
+{
+  // if ch is 
+  char * point = NULL;
+  int index = 0;
+  while (str[index] != '\0')
+    {
+      if (str[index] == ch)
+	{ 
+	  point = (char *)&str[index];
+	}
+      index ++;
+    }
+  if (ch == '\0')
+    {
+     point = (char *)&str[index];
+     return point;
+    } 
+  return point;
+}
+
+char * my_strcpy(char * dest, const char * src)
+{
+  int index = 0;
+  while (str[index] != '\0')
+    {
+      dest[index] = str[index];
+      index ++;
+    }
+  dest[index] = '\0';
+  return dest;
+}
+
