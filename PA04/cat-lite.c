319cb26140621c0b7c41f289ef2ae254fb7f1334
Igal Flegmann Sandler <iflegman@ecegrid-thin6.ecn.purdue.edu>
2014-10-02 18:52:32 -0400

    i dont want to delete everything again

--- PA04/cat-lite.c
+++ PA04/cat-lite.c
@@ -10,24 +10,30 @@ int main(int argc, char * * argv)
    // We want some variables to store which "switches" were set
    int showHelp = FALSE;
    int ch;
+   FILE * fptr;
+   int estanda = FALSE;
    int index2 = 1;
    int ind = 1; // we always skip 0, which is the program path
    for( ; ind < argc; ++ind) {
       if(strcmp(argv[ind], "--help") == 0) 
          showHelp = TRUE;
+      if (argc < 2)
+	estanda = TRUE;
+      if (strcmp(argv[ind], "-") == 0)
+	estanda = TRUE;
    }
    if (showHelp){
-     printf("Usage: cat-lite [--help] [FILE]...\n
-With no FILE, or when FILE is -, read standard input.\n\n
-Examples:\n
-  cat-lite README   Print the file README to standard output.\n
-  cat-lite f - g    Print f's contents, then standard input, \n
-                    then g's contents.\n
-  cat-lite          Copy standard input to standard output.\n");
+     printf("Usage: cat-lite [--help] [FILE]...\n");
+     printf("With no FILE, or when FILE is -, read standard input.\n\nExamples:\n  cat-lite README   Print the file README to standard output.\n  cat-lite f - g    Print f's contents, then standard input, \n                    then g's contents.\n  cat-lite          Copy standard input to standard output.\n");
      return EXIT_SUCCESS;
    }
    for( ; index2 < argc; ++index2) {
-     fptr = fopen(argv[index2], "r");
+     if (strcmp(argv[index2], "-") == 0)
+       {
+	 fptr = stdin;
+       }
+     else
+       fptr = fopen(argv[index2], "r");
      if(fptr == NULL){
        printf("cat cannot open %s\n", argv[index2]);
        return EXIT_FAILURE;
@@ -36,9 +42,19 @@ Examples:\n
        {
 	 ch = fgetc(fptr);
 	 if(ch != EOF)
-	   printf("%c");
+	   fprintf(stdout, "%c",ch);
        }while(ch != EOF);
      fclose(fptr);
-     return EXIT_SUCCESS;
    }
+if (argc == 1)
+       {
+	 fptr = stdin;
+	 do
+       {
+	 ch = fgetc(fptr);
+	 if(ch != EOF)
+	   fprintf(stdout, "%c",ch);
+       }while(ch != EOF);
+       }
+   return EXIT_SUCCESS;
 }
