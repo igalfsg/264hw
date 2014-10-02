a741e556684b31f772257db989f9f7dc3a077355
Igal Flegmann Sandler <iflegman@ecegrid-thin6.ecn.purdue.edu>
2014-10-02 18:52:03 -0400

    second time arround cause deleted everything

---
+++ PA04/grep-lite.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TRUE 1
+#define FALSE 0
+
+int main(int argc, char * * argv)
+{
+   // We want some variables to store which "switches" were set
+   int showHelp = FALSE;
+   int inver = FALSE;
+   int quiet = FALSE;
+   int num = FALSE;
+   int line = 1;
+   char buffer[2001];
+   int badSwitch = FALSE;
+   int r = 1;
+
+   // Loop through the arguments, looking for our switches...
+   int ind = 1; // we always skip 0, which is the program path
+   for( ; ind < argc; ++ind) {
+      if(strcmp(argv[ind], "--help") == 0) 
+         showHelp = TRUE;
+      if(strcmp(argv[ind][0], "-") == 0 && ind == (argc - 1))
+          badSwitch = TRUE;
+      if(strcmp(argv[ind], "-n") == 0 || strcmp (argv[ind],"--line-number"))
+	num = TRUE;
+      if(strcmp(argv[ind], "-q") == 0 || strcmp (argv[ind],"--quiet"))
+	quiet = TRUE;
+      if(strcmp(argv[ind], "-i") == 0 || strcmp (argv[ind],"--invert-match"))
+	inver = TRUE;
+     
+   }
+   // If --help is passed, you usually show help and do nothing else
+   if(showHelp) {
+      printf("Usage: ./icecream OPTIONS...\n"
+             "   -i, --serve-icecream  Not as exciting as it sounds\n"
+	     "\n");
+      return EXIT_SUCCESS;
+   }
+   // Usually you want to bail if the switches aren't perfect...
+   // because this helps developers make more reliable software.
+   if(badSwitch) {
+      fprintf(stderr, "Aborting...\n");
+      return 2;
+   }
+
+
+
+   return r;
+}
+
+
