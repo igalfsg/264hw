258741870c655208821e0e3ddb65c5125d6dd2ac
Igal Flegmann Sandler <iflegman@ecegrid-thin6.ecn.purdue.edu>
2014-10-27 21:37:41 -0400

    almost all functions done

--- PA08/answer08.c
+++ PA08/answer08.c
@@ -32,4 +32,38 @@ void List_Destroy (List * list){
 
 int List_length (List * list)
 {
-  int count = 
+  int count = 0;
+  if (list != NULL)
+    count++;
+  list = list->next;
+  return count;
+}
+
+
+List * List_sort (List * list, int (*compar)(const char *, const char*))
+{
+  List * itr = list;
+  List * newH = list->next;
+  int i;
+  int len = List_length (list);
+  if (len <= 1)
+    return list;
+  //devide the list
+  for(i=1; i < (len / 2); ++i)
+    {
+      itr = itr->next;
+      newH = newH->next;
+    }
+  itr->next = NULL;
+
+  //sort
+  newH = List_sort (newH, compare);///IDK how to pass compare
+  itr = List_sort (list, compare);///IDK how to pass compare
+
+  //merge
+  list = List_merge (newH, itr, compare);//IDK how to pass compare
+
+  return list;
+
+}
+  
