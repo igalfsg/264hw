63a9cb8b721b03cd563d1f39f861053d932da045
Igal Flegmann Sandler <iflegman@ecegrid-thin4.ecn.purdue.edu>
2014-12-11 17:57:53 -0500

    done 264!

--- PA12/answer12.c
+++ PA12/answer12.c
@@ -3,13 +3,10 @@
 #include <string.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <math.h>
 
-
-
-
-uint128 alphaTou128  (const char * str)
+uint128 alphaTou128(const char * str)
 {
-  uint128 multiplier = 1;
   uint128 indx = 0;
   uint128 ret = 0;
   while (str[indx] == ' ')
@@ -17,19 +14,12 @@ uint128 alphaTou128  (const char * str)
       indx++;
     }
 
-  while ((str[indx] >= '0' && str[indx] <= '9') || str[indx] == ' ')
+  while ((str[indx] >= '0' && str[indx] <= '9'))
     {
-      if(str[indx] != ' ')
-	{
-	  ret *= 10;
-	  ret += str[indx];
-	  ret -= 48;
-	}
+      ret *= 10;
+      ret += str[indx] - '0';	
       indx++;
     }
-  if (ret == 0 && str[indx] != '\0')
-    return 0;
-  ret = ret * multiplier;
   return ret;
 }
 
@@ -42,7 +32,7 @@ uint128 alphaTou128  (const char * str)
  */
 char * u128ToString(uint128 value)
 {
-  uint128 size = 0;
+  int size = 0;
   if(value == 0)
     size = 1;
   uint128 val2 = value;
@@ -53,7 +43,7 @@ char * u128ToString(uint128 value)
     }
   char * numb = malloc(sizeof(char) * (size + 1));
   numb[size] = '\0';
-  size = size -1;
+  size = size - 1;
   for (; size >= 0; size--)
   {
     numb[size] = val2 % 10 + '0';
@@ -72,55 +62,78 @@ char * u128ToString(uint128 value)
  *
  * Good luck!
  */
-void *  isprime (uint128 num)
- {
-   uint128 ind;
-   uint128 count = 0;
-   for(ind =  2; ind < num; ind++)
-     {
-       if (num % ind == 0)
-	 {
-	   count++;
-	 }
-     }
-   return count;
- }
 
 typedef struct paramblock {
   uint128 start;
   uint128 stup;
   uint128 number;
-  int prime;
+  int * prime;
 
 }block;
+
+void *  isprime (void * num_arg)
+ {
+   block *  numptr = (block *) num_arg;
+   block num = * numptr;
+   uint128 ind;
+   for(ind =  num.start; ind <= num.stup; ind++)
+     {
+       if (num.number % ind == 0)
+	 {
+	   * num.prime  = 0;
+	 }
+     }
+   return NULL;
+ }
+
 int primalityTestParallel(uint128 value, int n_threads)
 {
   int ind;
   int rtv;
-  int prime = 0;
+  int prime = 1;
+  if(value <= 1)
+    {
+      return 0;
+    }
+  if(value == 2)
+    {
+      return 1;
+    }
+  uint128 top = sqrt(value) + 1;
+  block b [n_threads];
+  uint128 num = 2;
+  uint128 topr;
+  uint128 range = top / n_threads;
   for (ind = 0; ind< n_threads; ind++)
     {
-      block b;
+      topr = num + range;
+      b[ind].start = num;
+      b[ind].stup = topr;
+      b[ind].number = value;
+      b[ind].prime = &prime;
     }
   pthread_t tid[n_threads];
   for (ind = 0; ind < n_threads; ind ++)
     {
-      //rtv = pthread_create( &tid[ind],NULL , , (void *)  );
-      
+      rtv = pthread_create( &tid[ind],NULL ,isprime , (void *)&b[ind]  );
     }
-  return 1;
+  for(ind = 0; ind < n_threads; ind++)
+    {
+      rtv = pthread_join (tid[ind],NULL);
+    }
+  return prime;
 }
 
-int main(int argc, char * * argv)
-{
-    const char * str = "340282366920938463463374607431768211455";
-    uint128 w = 0//alphaTou128(str);
-    char * w_str = u128ToString(w);
-    printf("Biiigggg number: %s\n", w_str);
-    if(strcmp(str, w_str) != 0)
-       printf("ERROR!, expected %s\n", str);
-    free(w_str);
-    return EXIT_SUCCESS;
-}
+/* int main(int argc, char * * argv) */
+/* { */
+/*     const char * str = "340282366920938463463374607431768211455"; */
+/*     uint128 w = alphaTou128(str); */
+/*     char * w_str = u128ToString(w); */
+/*     printf("Biiigggg number: %s\n", w_str); */
+/*     if(strcmp(str, w_str) != 0) */
+/*        printf("ERROR!, expected %s\n", str); */
+/*     free(w_str); */
+/*     return EXIT_SUCCESS; */
+/* } */
 
 
